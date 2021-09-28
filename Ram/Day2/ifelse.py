3ac92fec9f8ddf6943ec2468f0658dfa6b1c8517
ramakrishna <chimmaniramakrishna25@gmail.com>
2021-09-28 08:48:08 +0530

    Some changes in ifelse

--- Ram/Day2/ifelse.py
+++ Ram/Day2/ifelse.py
@@ -3,7 +3,7 @@ if(age>0):
     if(age<18):
         print("Given person is minor")
     elif(age>=18 and age<25):
-        print("Given person is Young age")
+        print("You are Young age")
     elif(age>=25 and age<60):
         print("Given person is middle age")
     else:
