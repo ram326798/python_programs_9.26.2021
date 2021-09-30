05abf449c1f613bbcc3676e94a895c83e3a403c7
Bandisula <nbandisu@amazon.com>
2021-09-30 09:49:21 +0530

    Programme_1

---
+++ Neelima/Day3/Practice.py
@@ -0,0 +1,6 @@
+amount=float(input("Enter the value of amount: "))
+balance=10000
+if(amount!=0 and amount>0):
+    print("Credited_amount",amount+balance)
+else:
+    print("amount should be greater than zero")
