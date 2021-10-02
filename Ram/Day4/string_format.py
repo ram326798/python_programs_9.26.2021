4e9fe30dcfa1c837e12a6abd54fb5b68a7881729
ramakrishna <chimmaniramakrishna25@gmail.com>
2021-10-02 16:15:06 +0530

    string_format and list

--- Ram/Day4/string_format.py
+++ Ram/Day4/string_format.py
@@ -1,9 +1,11 @@
 #Using simple format method
-"""
+
 price=int(input("Enter price :"))
-txt="price of dell is {}"
-print(txt.format(price))
-"""
+# txt="price of dell is {}"
+# print(txt.format(price))   
+# #or
+print(f"price of dell is {price}")# if i give f"" i need to give variable name index and empty bracket won't be accepted
+
 
 #Converting integer to float with specific round value
 """
@@ -24,10 +26,11 @@ print(txt.format(pri=price,quan=quantity,item=itemName))
 # s1="""Hi Welcome coding world 
 #       you are coding python"""
 # print("S1 string :",s1)
-
+"""
 #variables assigning using index position
 price=int(input("Enter price :"))
 quantity=int(input("Enter Quantity :"))
 itemName=input("Enter Item Name :")
 txt="price of {2} is {0} and quantity required is {1}"
 print(txt.format(price,quantity,itemName))
+"""
