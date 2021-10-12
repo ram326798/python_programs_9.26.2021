a093310b2882cbf4255d2d68e0fd9cb87c0c5f5f
VARALAKSHMI UMMADISETTI <varalakshmi1472000@gmail.com>
2021-10-12 15:33:32 +0530

    operators

---
+++ varalakshmi/operators.py
@@ -0,0 +1,42 @@
+#arthimetic operators
+"""
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+if b!=0:
+    print("sum of a & b:","float value",float(a+b), "and int value" ,int(a+b))
+    print("subtraction of a & b:","float value",float(a-b), "and int value" ,int(a-b))
+    print("multiplication of a & b:","float value",float(a*b), "and int value" ,int(a*b))
+    print("exponential of a & b:","float value",float(a**b), "and int value" ,int(a**b))
+    print("division of a & b:","float value",float(a//b), "and int value" ,int(a//b))
+    print("floor division of a & b:","float value",float(a/b), "and int value" ,int(a/b))
+    print("modulo of a & b:","float value",float(a%b), "and int value" ,int(a%b))
+"""
+#comparison operators
+"""
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+if b==a:
+    print("b equals to a",b==a)
+elif b>a:
+    print("b is greater than a",b>a)
+else:
+    print("b is less than a",b<a)
+"""
+#assignment operators
+"""
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+for d in range (b):
+    d-=b
+    print("c value",d)
+"""
+"""
+b=int(input("enter b value:"))
+for d in range (b):
+    d+=b
+    print("c value",d)
+"""
+b=int(input("enter b value:"))
+for d in range (b):
+    d=b*2
+    print("c value",d)
